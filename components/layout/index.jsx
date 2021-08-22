1f55b2ce9eb105ae08c84c2320d4a9e49468f58f
Kevin Nguyen <me@kndwin.dev>
2021-08-23 08:58:24 +1000

    feat: added anxiety and me page

--- components/layout/index.jsx
+++ components/layout/index.jsx
@@ -3,21 +3,19 @@ import Link from "next/link";
 const Layout = ({ children }) => {
   return (
     <div
-      className="flex flex-col items-center 
-				justify-center min-h-screen py-2"
+      className="flex flex-col items-center justify-center min-h-screen py-2"
     >
       <header className="w-full max-w-2xl color-primary text-center
-				mt-16 mb-4 relative flex items-center justify-center">
-				<h2 className="text-5xl absolute left-4 font-beauty">tina dairy</h2>
+      <header className="relative flex items-center justify-center w-full max-w-2xl mt-16 mb-4 text-center color-primary">
         <Link  href="/">
-					<p className="font-recoleta cursor-pointer mr-4">
+				<h2 className="absolute text-5xl left-4 font-beauty">tina dairy</h2>
 						work
-					</p>
+					<p className="mr-4 cursor-pointer font-recoleta">
 				</Link>
         <Link className='ml-4' href="/about">
 					<p className="font-recoleta cursor-pointer">
 						about
-					</p>
+					<p className="cursor-pointer font-recoleta">
 				</Link>
       </header>
       {children}
--- pages/projects/anxiety-and-me.jsx
+++ pages/projects/anxiety-and-me.jsx
@@ -1,13 +1,43 @@
-import Layout from "../../components/layout"
+import Layout from "../../components/layout";
+import Image from "next/image";
+
+import FrontCover from "../../public/anxiety-and-me/Anxiety and Me Front.png";
 
 const AnxietyAndMe = (props) => {
-	return (
-		<Layout>
-			<main className='flex-1'>
-				<h1 className="font-recoleta text-3xl">Anxiety and Me</h1>
-			</main>
-		</Layout>
-	)
-}
+  return (
+    <Layout>
+      <main className="container flex-1 w-full max-w-2xl">
+				<div className="flex flex-col-reverse w-full px-10 my-8 sm:flex-row" 
+					style={{ height: 'fit-content'}}>
+          <Image
+            src={FrontCover}
+            placeholder="blur"
+            height={778}
+            width={529}
+            layout='intrinsic'
+            className="object-cover"
+          />
+					<div className='w-4/5 pt-8 ml-8'>
+          <h1 className="text-3xl color-primary font-recoleta">Anxiety and Me</h1>
+						<p className='font-recoleta color-primary'>Book Cover design</p>
+						<div className='mt-4 mb-2'>
+							<p className='font-recoleta color-primary'>Toolkit</p>
+							<p className='font-recoleta color-primary'>Timeframe: 2 weeks</p>
+						</div>
+						<p className='text-sm font-recoleta'>
+							This bookcover was designed for Matthias Media for author Guan Un. 
+							The brief required a short turnover but I had creative freedom in 
+							the direction I thought best suited the books theme and content.
+							<br />
+							<br />
+							I mainly  used procreate to draw the main illustration and  drew 
+							inspiration from various different Japanese artists.
+						</p>
+					</div>
+        </div>
+      </main>
+    </Layout>
+  );
+};
 
-export default AnxietyAndMe
+export default AnxietyAndMe;
