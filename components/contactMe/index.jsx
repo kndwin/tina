9742b549de9c8383c08002fec3170ef4a7081ca2
Kevin <kevin.nguyen@sauce.app>
2025-06-12 19:33:27 +1000

    Update photo and resume and email

--- components/contactMe/index.jsx
+++ components/contactMe/index.jsx
@@ -2,30 +2,28 @@ import { SiGmail, SiLinkedin, SiInstagram } from "react-icons/si";
 import DownloadIcon from "components/svgs/download.svg";
 
 const ContactMe = (props) => {
-	return (
-		<div className="flex items-center mt-4">
-			<p className="mr-4 text-xl font-recoleta color-primary">
-				Contact me
-			</p>
-			<div className="flex items-center justify-center">
-				<a href="mailto:tinadairy@gmail.com">
-					<SiGmail className="cursor-pointer color-primary" />
-				</a>
-				<a href="https://www.linkedin.com/in/tina-nguyen-158931207/">
-					<SiLinkedin className="mx-2 cursor-pointer color-primary" />
-				</a>
-				<a href="https://www.instagram.com/thetinadairy/">
-					<SiInstagram className="cursor-pointer color-primary" />
-				</a>
-				<a href="/TinaNguyen.pdf">
-					<button className="flex items-center px-2 py-1 ml-4 text-sm buttonPrimary border-primary color-primary font-recoleta rounded-md">
-						Resume
-						<DownloadIcon className="ml-2" />
-					</button>
-				</a>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className="flex items-center mt-4">
+      <p className="mr-4 text-xl font-recoleta color-primary">Contact me</p>
+      <div className="flex items-center justify-center">
+        <a href="mailto:tinaxingdesign@gmail.com">
+          <SiGmail className="cursor-pointer color-primary" />
+        </a>
+        <a href="https://www.linkedin.com/in/tina-nguyen-158931207/">
+          <SiLinkedin className="mx-2 cursor-pointer color-primary" />
+        </a>
+        <a href="https://www.instagram.com/thetinadairy/">
+          <SiInstagram className="cursor-pointer color-primary" />
+        </a>
+        <a href="/TinaNguyen.pdf">
+          <button className="flex items-center px-2 py-1 ml-4 text-sm buttonPrimary border-primary color-primary font-recoleta rounded-md">
+            Resume
+            <DownloadIcon className="ml-2" />
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default ContactMe
+export default ContactMe;
--- pages/about.jsx
+++ pages/about.jsx
@@ -1,7 +1,7 @@
 import ContactMe from "components/contactMe";
 import Image from "next/image";
 
-import Profile from "../public/profile.png";
+import Profile from "../public/profile.jpeg";
 
 const About = (props) => {
   const tagline = `Hi there! My name is Tina 😊 and I’ve been designing for quite some time.  I’m always looking for new skills to learn.
@@ -11,13 +11,15 @@ In my spare time, I like to draw, rollerskate and am a big tea drinker (black te
 If you have a project that you would like to bring to life then I would love  to collaborate with you in doing so.`;
 
   return (
-    <div className='flex flex-col items-start flex-1 pt-8'>
+    <div className="flex flex-col items-start flex-1 pt-8">
       <h1 className="w-full my-8 text-7xl font-recoleta color-primary">
         about me
       </h1>
       <div className="flex flex-col justify-center w-full h-full sm:flex-row">
         <div className="flex-col justify-between flex-1 h-full">
-          <p className="mb-12 text-xl whitespace-pre-wrap font-manrope">{tagline}</p>
+          <p className="mb-12 text-xl whitespace-pre-wrap font-manrope">
+            {tagline}
+          </p>
           <ContactMe />
         </div>
         <div className="flex-1 w-full h-full mt-12 sm:mt-0 sm:ml-16">
